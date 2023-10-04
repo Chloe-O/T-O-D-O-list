f0665539513b8880da67286216a8df4fba931deb
Chloe-O <chloeosbourne@yahoo.co.uk>
2023-10-04 19:37:03 +0100

    count list items function

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 let listItems = document.querySelectorAll("li");
 let checkboxToggle = document.querySelectorAll("li .checkbox");
-let itemsNum = document.getElementById("itemsNum");
+const itemsNum = document.getElementById("itemsNum");
+const clearCompleted = document.getElementById("clearCompleted");
 
-let listItemsNo = 0;
+const toDoArray = [];
 
-listItems.forEach((li) => {
-  listItemsNo++;
-  itemsNum.innerHTML = listItemsNo;
-});
+//Input new list item
 
+//Count list items
+function countListItems() {
+  let listItemsNo = 0;
+
+  listItems.forEach((li) => {
+    if (li.classList.contains("li-complete")) {
+      listItemsNo++;
+      itemsNum.innerHTML = listItemsNo;
+    } else {
+      itemsNum.innerHTML = 0;
+    }
+  });
+}
+
+//Toggle completed on click
 checkboxToggle.forEach((ct) => {
   ct.addEventListener("click", () => {
     ct.classList.toggle("checkbox-checked");
+    ct.parentNode.classList.toggle("li-complete");
+    countListItems();
   });
 });
+
+//Clear completed items
+clearCompleted.addEventListener("click", () => {
+  listItems.forEach((i) => {
+    if (i.classList.contains("li-complete")) {
+      console.log(i);
+    }
+  });
+});
+
+countListItems();
