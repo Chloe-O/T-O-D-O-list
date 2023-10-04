3704a2c22a851f8c31b57c7311e6e4463d0c155e
Chloe-O <chloeosbourne@yahoo.co.uk>
2023-10-04 20:49:17 +0100

    working on push list items to DOM

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+let todoList = document.getElementById("todoList");
 let listItems = document.querySelectorAll("li");
 let checkboxToggle = document.querySelectorAll("li .checkbox");
 const itemsNum = document.getElementById("itemsNum");
 const clearCompleted = document.getElementById("clearCompleted");
+const addItem = document.getElementById("addItemBtn");
+const todoInput = document.getElementById("todoInput");
 
 const toDoArray = [];
-
-//Input new list item
+const toDoArrayHTML = [];
 
 //Count list items
 function countListItems() {
   let listItemsNo = 0;
 
   listItems.forEach((li) => {
-    if ( !(li.classList.contains("li-complete")) ) {
+    if (!li.classList.contains("li-complete")) {
       listItemsNo++;
       itemsNum.innerHTML = listItemsNo;
     } else {
@@ -20,6 +22,30 @@ function countListItems() {
     }
   });
 }
+
+//Input new list item
+addItem.addEventListener("click", () => {
+  if (todoInput.value) {
+    toDoArray.push(todoInput.value);
+    // createHTMLitem();
+    todoInput.value = "";
+    countListItems();
+  }
+});
+
+//Create HTML item
+function createHTMLitem() {
+  toDoArray.forEach((ai) => {
+    ai = `<li><div class="checkbox"></div>${ai}</li>`;
+    toDoArrayHTML.push(ai);
+
+  });
+}
+
+// //Push HTML items into DOM
+// function displayHTMLitem() {
+
+// }
 
 //Toggle completed on click
 checkboxToggle.forEach((ct) => {
